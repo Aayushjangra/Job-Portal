3ee60c07a1070b80fc2b027a7f836b2c2a1a7475
Aayush Jangra <118102480+Aayushjangra@users.noreply.github.com>
2024-10-20 20:34:40 +0530

    getcompanybyid and getcompany and update company  api call

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import { Company } from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const registerCompany = async (req, res) => {
     try {
@@ -80,10 +82,14 @@ export const updateCompany = async (req, res) => {
         const { name, description, website, location } = req.body;
  
         const file = req.file;
-        // idhar cloudinary ayega
+        
+
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
         
     
-        const updateData = { name, description, website, location };
+        const updateData = { name, description, website, location, logo };
 
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
 
@@ -95,7 +101,8 @@ export const updateCompany = async (req, res) => {
         }
         return res.status(200).json({
             message:"Company information updated.",
-            success:true
+        
+            success:true,
         })
 
     } catch (error) {
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,26 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log('mongodb connected successfully');
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// export default connectDB;
+
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
 export default connectDB;
+
